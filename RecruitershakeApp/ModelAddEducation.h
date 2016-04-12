0c0aacc03446e5672dd2f8e23d4bf34aa110ee41
admin <admin@admins-imac.local>
2016-04-12 12:14:33 +0530

    add education

---
+++ RecruitershakeApp/ModelAddEducation.h
@@ -0,0 +1,13 @@
+//
+//  ModelAddEducation.h
+//  RecruitershakeApp
+//
+//  Created by admin on 12/04/16.
+//  Copyright © 2016 Xigmapro. All rights reserved.
+//
+
+#import "ModelBaseClass.h"
+
+@interface ModelAddEducation : ModelBaseClass
+
+@end
---
+++ RecruitershakeApp/ModelAddEducation.m
@@ -0,0 +1,13 @@
+//
+//  ModelAddEducation.m
+//  RecruitershakeApp
+//
+//  Created by admin on 12/04/16.
+//  Copyright © 2016 Xigmapro. All rights reserved.
+//
+
+#import "ModelAddEducation.h"
+
+@implementation ModelAddEducation
+
+@end
